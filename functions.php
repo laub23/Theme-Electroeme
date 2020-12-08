ed5d19c86e70d2de07be24cb20bedb0664a8c026
laura <laura.brager@edu.univ-fcomte.fr>
2020-12-08 11:15:40 +0100

    CSS
    css partie 1

--- functions.php
+++ functions.php
@@ -6,5 +6,7 @@
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
 wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+// enqueue child styles
+wp_enqueue_style('child-theme', get_stylesheet_directory_uri() .'/style.css', array('parent-style'));
 }
 ?>
